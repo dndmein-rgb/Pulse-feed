02ffa60f892edf435ee4bdf8ac4b4d3c0b2b5775
Divyanshu <dnd.mein@gmail.com>
2025-11-21 00:26:53 +0530

    1-notification routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
  import authRoutes from './routes/auth.route.js';
  import userRoutes from './routes/user.route.js';
  import postRoutes from './routes/post.route.js';
+ import notificationRoutes from './routes/notification.route.js';
  import { connectDb } from './db/connectDb.js';
  import {v2 as cloudinary} from 'cloudinary';
 
@@ -26,6 +27,7 @@ cloudinary.config({
  app.use('/api/auth',authRoutes)
  app.use('/api/users',userRoutes)
  app.use('/api/posts',postRoutes)
+ app.use('/api/notifications', notificationRoutes);
 
  app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
