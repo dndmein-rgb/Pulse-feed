90b2a38246ff94a1b472e4d50f0d24873e637340
Divyanshu <dnd.mein@gmail.com>
2025-11-22 17:35:35 +0530

    3-delete post added

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -10,34 +10,36 @@ const HomePage = () => {
 		<>
 			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
 				{/* Header */}
-				<div className='flex w-full border-b border-gray-700'>
-					<div
-						className={
-							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-						}
-						onClick={() => setFeedType("forYou")}
-					>
-						For you
-						{feedType === "forYou" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-					<div
-						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
-						onClick={() => setFeedType("following")}
-					>
-						Following
-						{feedType === "following" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-				</div>
+		<div className="flex w-full border-b border-gray-700">
+  <div
+    className={`flex justify-center flex-1 p-3 cursor-pointer relative transition duration-300
+    ${feedType === "forYou" ? "text-primary font-semibold" : "text-gray-400 hover:bg-gray-800"}`}
+    onClick={() => setFeedType("forYou")}
+  >
+    For you
+    {feedType === "forYou" && (
+      <div className="absolute bottom-0 h-1 w-10 bg-primary rounded-full"></div>
+    )}
+  </div>
+
+  <div
+    className={`flex justify-center flex-1 p-3 cursor-pointer relative transition duration-300
+    ${feedType === "following" ? "text-primary font-semibold" : "text-gray-400 hover:bg-gray-800"}`}
+    onClick={() => setFeedType("following")}
+  >
+    Following
+    {feedType === "following" && (
+      <div className="absolute bottom-0 h-1 w-10 bg-primary rounded-full"></div>
+    )}
+  </div>
+</div>
+
 
 				{/*  CREATE POST INPUT */}
 				<CreatePost />
 
 				{/* POSTS */}
-				<Posts />
+				<Posts  feedType={feedType}/>
 			</div>
 		</>
 	);
